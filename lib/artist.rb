6bb6222e5f098c2d459622ad9858f881e0a2435d
Benjamin Cheng <bencheng200@gmail.com>
2020-11-21 03:34:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,7 +16,7 @@ class Artist
     @@all << self     #saves all created instances of Artist class in #@@all class variable
   end
 
-  def all      #class method all returns all instances of Artist class
+  def self.all      #class method all returns all instances of Artist class
     @@all
   end
 
