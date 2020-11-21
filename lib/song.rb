2708435c2d0da90824d72807c10b09bd85604d1c
Benjamin Cheng <bencheng200@gmail.com>
2020-11-21 03:52:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,10 @@ class Song
   end
 
   def artist_name=(name)
+    binding.pry
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
-    binding.pry
+
   end
 
   def self.new_by_filename(file)
